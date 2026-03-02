42c4b5c4451b7bec121c662de77ef13161761d5f
sindhupriya-ren <sindhupriya.reni@gmail.com>
2026-03-02 14:59:35 +0530

    msg

--- IV - SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST01/task.py
+++ IV - SEM/Python/Assignments/M01_Language_&_Programming_Basics/AST01/task.py
@@ -1,5 +1,12 @@
 def Ticket_Pricing(n: int) -> int:
-   pass
+   if n<5:
+      return("Free")
+   elif(n>=5 and n<=17):
+      return(10)
+   elif(n>=18 and n<=64):
+      return(20)
+   else:
+      return(15)
 
 
 if __name__ == '__main__':
